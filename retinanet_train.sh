9fa67a0837305acb85d3007357bfdc90873b7d90
Rich Sharp <richpsharp@gmail.com>
2020-03-09 15:02:33 -0700

    getting python path right

--- retinanet_train.sh
+++ retinanet_train.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+export PYTHONPATH=$PYTHONPATH:/usr/local/tf_models/
 MODEL_DIR=/usr/local/model_files
 python /usr/local/tf_models/official/vision/detection/main.py \
   --strategy_type=one_device \
   --num_cpus=1 \
   --model_dir="${MODEL_DIR?}" \
   --mode=train \
-  --config_file="my_retinanet.yaml"
+  --config_file="dams_retinanet.yaml"
