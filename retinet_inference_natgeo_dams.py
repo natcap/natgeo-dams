6354f0bce3ff84ecd6657d2342ff218ecf487d50
Rich Sharp <richpsharp@gmail.com>
2020-03-29 22:44:12 -0700

    all done with ZAF so guard against that

--- retinet_inference_natgeo_dams.py
+++ retinet_inference_natgeo_dams.py
@@ -791,6 +791,9 @@ def main():
         WHERE country_list LIKE "%ZAF%" AND processed=0
         ''', WORK_DATABASE_PATH, argument_list=[], fetch='all')
 
+    if work_grid_list is None:
+        work_grid_list = []
+
     work_grid_list.append(
         _execute_sqlite('''
             SELECT grid_id, lng_min, lat_min, lng_max, lat_max
