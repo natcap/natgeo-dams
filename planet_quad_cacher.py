eaeb3a25c2598af24a75a184de2f5c7bf17057c0
Rich Sharp <richpsharp@gmail.com>
2020-10-14 08:23:17 -0700

    it's /bin

--- planet_quad_cacher.py
+++ planet_quad_cacher.py
@@ -251,7 +251,7 @@ def fetch_quad(
 
         try:
             subprocess.run(
-                '/usr/local/gcloud-sdk/google-cloud-sdk/gsutil cp %s %s'
+                '/usr/local/gcloud-sdk/google-cloud-sdk/bin/gsutil cp %s %s'
                 % (local_quad_path, quad_uri), shell=True, check=True)
         except subprocess.CalledProcessError:
             LOGGER.warning('file might already exist')
